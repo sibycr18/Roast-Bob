685bcf0739d0a9903428ed698e8d2fdff588324e
Siby C.R <sibycr18@gmail.com>
2024-11-27 19:21:31 +0530

    added a tool used to commit messages in kafka queue

---
+++ tools/commit_message.py
@@ -0,0 +1,30 @@
+from kafka import KafkaConsumer, TopicPartition, OffsetAndMetadata
+import json
+
+# Define the consumer
+consumer = KafkaConsumer(
+    'twitter.mentions',
+    bootstrap_servers=['localhost:9092'],
+    value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+    group_id='twitter_roaster_group',
+    auto_offset_reset='latest',
+    enable_auto_commit=False  # Disable auto commit
+)
+
+# Consume messages
+for message in consumer:
+    item = json.dumps(message.value, indent=2)
+    print(f"Consumed message: {item}\n")
+
+    # Check for the desired condition
+    if message.value.get('id') == '1860861236134715764':
+        print("Condition matched, committing this message.")
+
+        # Get the TopicPartition for the current message
+        topic_partition = TopicPartition(message.topic, message.partition)
+        
+        # Commit the specific offset for this message
+        consumer.commit({
+            topic_partition: OffsetAndMetadata(message.offset + 1, '')
+        })
+        break
